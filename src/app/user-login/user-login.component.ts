9536b2a8f6dd7ab4731ba4b18f98d117fac8cd6d
yehorchernenko <lordo916@gmail.com>
2018-12-27 01:41:41 +0200

    Added local storage and redirect

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { LocalStorage } from '@ngx-pwa/local-storage';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login',
@@ -10,13 +12,36 @@ export class UserLoginComponent implements OnInit {
   email = '';
   password = '';
 
-  constructor(private api: ApiService) {}
+  constructor(private api: ApiService, protected localStorage: LocalStorage, private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.localStorage.getItem('user').subscribe((user: string) => {
+      if (user) {
+        const userObj = JSON.parse(user);
+
+        this.api.userLogin(userObj.email, userObj.password).subscribe(response => {
+          if (response.status === 200) {
+            this.router.navigate(['/admin']);
+          }
+        }, error => {
+          console.log(`Error ${error}`);
+        });
+      } else {
+        console.log(`Local storage error user is null`);
+      }
+    }, error => {
+      console.log(`Local storage error ${error}`);
+    });
+  }
 
   onSubmit() {
     this.api.userLogin(this.email, this.password).subscribe(response => {
-      console.log(response.status);
+      console.log(response);
+      if (response.status === 200) {
+        this.localStorage.setItem('user', JSON.stringify({email: this.email, password: this.password})).subscribe(() => {});
+        this.router.navigate(['/admin']);
+      }
+
     }, error => {
       console.log(`Error ${error}`);
     });
